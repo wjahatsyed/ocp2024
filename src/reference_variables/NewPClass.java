68c781275bc51ed688397b1695bf3fb30748ece8
Wajahat Adil <wajahatsyed1257@gmail.com>
2024-04-24 12:00:16 +0500

    reference varialbles and overloading

--- src/reference_variables/NewPClass.java
+++ src/reference_variables/NewPClass.java
@@ -3,3 +3,29 @@ package reference_variables;
 public class NewPClass {
 
 }
+class NewCClass extends NewPClass implements MyInterface{
+	public void myChildClassMethod() {
+		
+	}
+}
+
+interface MyInterface{
+	
+}
+
+class NewTestClass{
+	public static void main(String[] args) {
+		//Access a child class object through parent class
+		NewPClass newPClassReference = new NewCClass();
+		
+		//Access a child class object through its own reference
+		NewCClass newCClassReference = new NewCClass();
+		
+		//Access a child class object through an implemented interface reference
+		MyInterface myInterfaceReference = new NewCClass();
+		//below is not accessible
+		//myInterfaceReference.myChildClassMethod();
+		System.out.println("The method myChildClassMethod is not declared"
+				+ "in MyInterface");
+	}
+}
